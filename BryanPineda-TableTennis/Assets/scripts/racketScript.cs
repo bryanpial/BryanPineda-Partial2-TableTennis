10aec6c25bd9eff593db90fa740cd211846c4b4c
Bryan Styven Pineda Albarracin <brayanpial@gmail.com>
2018-10-09 21:55:15 -0500

    no message

---
+++ BryanPineda-TableTennis/Assets/scripts/racketScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class racketScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.right);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.back);
+        }
+
+    }
+}
